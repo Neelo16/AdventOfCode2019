7125403ed6acdf92c9a307de8558aaed44d368be
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2019-12-02 20:06:41 +0000

    Add metaprogramming

--- Day2/intcode.jl
+++ Day2/intcode.jl
@@ -1,13 +1,29 @@
 const INPUTFILE = joinpath(@__DIR__, "input.txt")
 input = parse.(Int, split(read(INPUTFILE, String), ","))
 
-function getop(opcode::Int)
-    if opcode == 1
-        +
-    elseif opcode == 2
-        *
-    elseif opcode == 99
-        :halt
+struct Instruction
+    opcode::Int
+    op
+    size::Int
+end
+
+macro defop(val, op, size)
+    quote
+        function $(esc(:fetch))(::Val{$(esc(val))})
+            Instruction($(esc(val)), $(esc(op)), $(esc(size)))
+        end
+    end
+end
+
+@defop(1, +, 3)
+@defop(2, *, 3)
+@defop(99, :halt, 0)
+
+valid_opcodes() = collect(map(m -> m.sig.types[2].parameters[1], methods(fetch, (Val,)).ms))
+
+function fetch(opcode::Int)
+    if opcode ∈ valid_opcodes()
+        fetch(Val(opcode))
     else
         error("Unexpected opcode $(opcode)")
     end
@@ -15,12 +31,23 @@ end
 
 incr(x) = x+1
 
-function runprogram(program::Vector{Int})
-    for ip in 1:4:length(program)
-        op = getop(program[ip])
-        op == :halt && break
-        a, b, dest = incr.(program[ip+1:ip+3]) # Input is zero-indexed, Julia is one-indexed
-        program[dest] = op(program[a], program[b])
+function runprogram!(program::Vector{Int})
+    ip = 1
+    while true
+        instruction = fetch(program[ip])
+        instruction.op == :halt && break
+        args = incr.(program[ip+1:ip+instruction.size]) # Input is zero-indexed, Julia is one-indexed
+        if length(args) > 1
+            args, dest = args[1:end-1], args[end]
+        elseif length(args) == 1
+            args, dest = [], first(args)
+        else
+            instruction.op()
+            @goto nextip
+        end
+        program[dest] = instruction.op([program[i] for i in args]...)
+        @label nextip
+        ip += 1 + instruction.size
     end
     program
 end
@@ -33,11 +60,11 @@ function prepare(program::Vector{Int}, noun::Int, verb::Int)
 end
 
 alarm = prepare(input, 12, 2)
-println("First half: $(runprogram(alarm)[1])")
+println("First half: $(runprogram!(alarm)[1])")
 
 for (noun, verb) in Iterators.product(0:99, 0:99)
     tentative = prepare(input, noun, verb)
-    runprogram(tentative)
+    runprogram!(tentative)
     if tentative[1] == 19690720
         println("Second half: $(100 * noun + verb)")
         break

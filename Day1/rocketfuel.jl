81b9109612d63e6e21e2618fbe996eaadd6d49a5
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2019-12-02 20:34:55 +0000

    Slightly simplify day 1 solution

--- Day1/rocketfuel.jl
+++ Day1/rocketfuel.jl
@@ -1,12 +1,12 @@
 const INPUTFILE = joinpath(@__DIR__, "input.txt")
 input = parse.(Int, readlines(INPUTFILE))
 
-fuel(mass)::Int = round(mass / 3, RoundDown) - 2
+fuel(mass)::Int = max(round(mass / 3, RoundDown) - 2, 0)
 
 println("First half: $(sum(fuel.(input)))")
 
 function totalfuel(mass, acc=0)::Int
-    if mass < 0 || fuel(mass) < 0
+    if mass == 0
         acc
     else
         totalfuel(fuel(mass), acc + fuel(mass))

64c67a65246bf5f8a368882e4f50b2b9ad954a51
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2019-12-08 15:28:27 +0000

    Add Day 8 solution

---
+++ Day8/image.jl
@@ -0,0 +1,37 @@
+const INPUTFILE = joinpath(@__DIR__, "input.txt")
+
+const IMAGE_WIDTH = 25
+const IMAGE_HEIGHT = 6
+
+pixels = parse.(Int, split(chomp(read(INPUTFILE, String)), ""))
+
+layers = collect(Iterators.partition(pixels, IMAGE_WIDTH * IMAGE_HEIGHT))
+
+istwo(x) = x == 2
+
+let mincount = typemax(Int), minlayer = nothing
+    for layer in layers
+        zerocount = count(iszero, layer)
+        if zerocount < mincount
+            mincount = zerocount
+            minlayer = layer
+        end
+    end
+    print("First half: $(count(isone, minlayer) * count(istwo, minlayer))")
+end
+
+let image = copy(layers[end])
+    for layer in layers[end-1:-1:1]
+        for (i, color) in enumerate(layer)
+            if color != 2
+                image[i] = color
+            end
+        end
+    end
+    for line in Iterators.partition(image, IMAGE_WIDTH)
+        for pixel in line
+            print(pixel == 1 ? '▩' : ' ')
+        end
+        println()
+    end
+end

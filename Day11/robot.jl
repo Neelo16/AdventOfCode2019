f1717454c789bbfc82ab59412e74b754a5074238
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2019-12-11 20:31:50 +0000

    Refactor Day 11 code to use Intcode module
    Additionally, refactored it to use a proper state pattern

---
+++ Day11/robot.jl
@@ -0,0 +1,60 @@
+using DataStructures
+using Intcode
+
+const INPUTFILE = joinpath(@__DIR__, "input.txt")
+
+inp = parse.(Int, split(read(INPUTFILE, String), ","))
+
+const PAINTING = 0
+const TURNING = 1
+
+const BLACK = 0
+const WHITE = 1
+
+function paint!(brain::Computer, startcolor=BLACK)
+    ship = DefaultDict{Complex,Int}(0)
+    direction = -im
+    position = 0
+    state = PAINTING
+    ip = 1
+    ship[position] = startcolor
+    setstate! = (state) -> defop!(brain, OUTPUT_OPCODE, state, 1, writemem=false)
+    paintingstate = (output) -> begin
+        ship[position] = output
+        state = TURNING
+        setstate!(turningstate)
+    end
+    turningstate =  (output) -> begin
+        direction *= output == 1 ? im : -im
+        position += direction
+        state = PAINTING
+        setstate!(paintingstate)
+    end
+    initialstate = paintingstate
+    defop!(brain, INPUT_OPCODE, () -> ship[position], 1)
+    setstate!(initialstate)
+    runprogram!(brain)
+    ship
+end
+
+function display_hull(ship::DefaultDict{Complex,Int})
+    coordinates = keys(ship)
+    min_x, max_x = minimum(real.(coordinates)), maximum(real.(coordinates))
+    min_y, max_y = minimum(imag.(coordinates)), maximum(imag.(coordinates))
+    for y in min_y:max_y
+        for x in min_x:max_x
+            pixel = ship[Complex(x, y)]
+            print(pixel == WHITE ? '▩' : ' ')
+        end
+        println()
+    end
+end
+
+let program = Computer(inp), ship = paint!(program, BLACK)
+    println("First half: $(length(keys(ship)))")
+end
+
+let program = Computer(inp), ship = paint!(program, WHITE)
+    println("Second half: ")
+    display_hull(ship)
+end

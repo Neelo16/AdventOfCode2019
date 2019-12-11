e7a2edcef5c7ff489e7fda804a8aef9e4d4eb53c
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2019-12-11 20:39:11 +0000

    Refactor Day 5 to use Intcode module

--- Day5/intcode.jl
+++ Day5/intcode.jl
@@ -1,107 +1,16 @@
+using Intcode
+
 const INPUTFILE = joinpath(@__DIR__, "input.txt")
+const inp = parse.(Int, split(read(INPUTFILE, String), ","))
 
-const POSITION = 0
-const IMMEDIATE = 1
-
-struct Instruction
-    opcode::Int
-    op
-    size::Int
-    output::Bool
-    jump::Bool
+let computer = Computer(inp)
+    defop!(computer, INPUT_OPCODE, () -> 1, 1)
+    println("First half: ")
+    runprogram!(computer)
 end
 
-struct InstructionPointer
-    ip::Int
+let computer = Computer(inp)
+    defop!(computer, INPUT_OPCODE, () -> 5, 1)
+    println("Second half: ")
+    runprogram!(computer)
 end
-
-const instructions = Dict{Int, Instruction}()
-
-function defop(opcode::Int, op, size::Int, output=true; jump=false)
-    instructions[opcode] = Instruction(opcode, op, size, output, jump)
-end
-
-function readinput()
-    print("Input: ")
-    parse(Int, readline(stdin))
-end
-
-defop(1, +, 3)
-defop(2, *, 3)
-defop(99, :halt, 0)
-
-defop(3, readinput, 1)
-defop(4, println, 1, false)
-
-jumpiftrue(cond, value, ip) = cond != 0 ? value + 1 : ip
-jumpiffalse(cond, value, ip) = cond == 0 ? value + 1 : ip
-lessthan(args...)::Int = <(args...)
-equals(args...)::Int = ==(args...)
-
-defop(5, jumpiftrue, 2, jump=true)
-defop(6, jumpiffalse, 2, jump=true)
-defop(7, lessthan, 3)
-defop(8, equals, 3)
-
-getopcode(opcode::Int) = opcode % 100
-valid_opcode(opcode) = opcode ∈ keys(instructions)
-
-function getparametermodes(opcode::Int)
-    instruction = fetch(opcode)
-    digits(opcode, pad=instruction.size + 2)[3:end]
-end
-
-function getargvalues(parametermodes, args, program)
-    values = []
-    for (arg, mode) in zip(args, parametermodes)
-        if mode == IMMEDIATE
-            push!(values, arg)
-        elseif mode == POSITION
-            push!(values, program[arg + 1]) # Input is zero-indexed, Julia is one-indexed
-        else
-            error("Unrecognized parameter mode $mode")
-        end
-    end
-    values
-end
-
-function fetch(instructioncode::Int)
-    opcode = getopcode(instructioncode)
-    if valid_opcode(opcode)
-        instructions[opcode]
-    else
-        error("Unexpected opcode $(opcode)")
-    end
-end
-
-function runprogram!(program::Vector{Int})
-    ip = 1
-    while true
-        instruction = fetch(program[ip])
-        instruction.op == :halt && break
-        args = program[ip+1:ip+instruction.size]
-        argvalues = getargvalues(getparametermodes(program[ip]), args, program)
-        if instruction.jump
-            newip = instruction.op(argvalues..., ip)
-            changed = newip != ip
-            ip = newip
-            changed && continue
-        elseif instruction.output
-            program[args[end] + 1] = instruction.op(argvalues[1:end-1]...)
-        else
-            instruction.op(argvalues...)
-        end
-        ip += 1 + instruction.size
-    end
-    program
-end
-
-println("First half: ")
-defop(3, () -> 1, 1)
-inp = parse.(Int, split(read(INPUTFILE, String), ","))
-runprogram!(inp)
-
-println("Second half: ")
-defop(3, () -> 5, 1)
-inp = parse.(Int, split(read(INPUTFILE, String), ","))
-runprogram!(inp)
